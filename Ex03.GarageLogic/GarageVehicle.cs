0c81431f87567e92577aefd235cb8b6d989e29ea
Omer Gery <omergery1996@gmail.com>
2021-05-08 18:07:22 +0300

    starting

---
+++ Ex03.GarageLogic/GarageVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class GarageVehicle
+    {
+        private string m_OwnerName;
+        private string m_PhoneNumber;
+        enum eFixState
+        {
+            BeingFixed,
+            Fixed,
+            Payed
+        }
+
+        private Vehicle m_Vehicle;
+    }
+}
