f0c90f286db1247eb2ff81f3dd6185cfa19b7272
Omer Gery <omergery1996@gmail.com>
2021-05-08 23:51:43 +0300

    .

---
+++ Ex03.GarageLogic/VehicleBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class VehicleBuilder
+    {
+        public enum eVehicleTypes
+        {
+            ElectricCar,
+            FuelCar,
+            ElectricBike,
+            FuelBike,
+            FuelTruck
+        }
+        
+    }
+}
