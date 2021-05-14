ef1cae68e3643450a67b18ca9e6a59d589856181
dolev7 <d.dolev7@gmail.com>
2021-05-14 17:50:13 +0300

    out of survival mode

---
+++ Ex03.GarageLogic/GarageEnums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class GarageEnums
+    {
+        public enum eFixState
+        {
+            BeingFixed = 1,
+            Fixed,
+            Payed
+        }
+
+        public enum eFuelType
+        {
+            Soler = 1,
+            Octan96,
+            Octan95,
+            Octan98
+        }
+
+        public enum eNumberOfDoors
+        {
+            Two = 2,
+            Three,
+            Four,
+            Five
+        }
+
+        public enum eColor
+        {
+            Red,
+            Silver,
+            White,
+            Black
+        }
+
+        public enum eBikeLicenceType
+        {
+            A,
+            B1,
+            AA,
+            BB
+        }
+    }
+}
